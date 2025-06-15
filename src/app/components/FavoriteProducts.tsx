8266c326c5f98e63aef28c06b3bfabeefe57f04e
shubhamvinnovate <shubham.sutar@vinnovatetechnologies.com>
2025-06-15 21:25:17 +0530

    UI fixes

--- src/app/components/FavoriteProducts.tsx
+++ src/app/components/FavoriteProducts.tsx
@@ -1,46 +1,46 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-'use client';
-import React, { useEffect, useState } from 'react';
-import { Col, Container, Row } from 'reactstrap';
-import ProductCard from './ProductCard';
-import styles from './favorite.module.css';
+"use client"
+import React, { useEffect, useState } from "react"
+import { Col, Container, Row } from "reactstrap"
+import ProductCard from "./ProductCard"
+import styles from "./favorite.module.css"
 
 const FavoriteProducts = () => {
-  const [productList, setProductList] = useState<any>([]);
+  const [productList, setProductList] = useState<any>([])
   useEffect(() => {
     setProductList([
       {
-        image: '/model_1.png',
-        title: 'Smooth Cloth',
-        price: 28
+        image: "/model_1.png",
+        title: "Smooth Cloth",
+        price: 28,
       },
       {
-        image: '/model_5.png',
-        title: 'Denim Jacket',
-        price: 28
+        image: "/model_5.png",
+        title: "Denim Jacket",
+        price: 28,
       },
       {
-        image: '/model_7.png',
-        title: 'Yellow Jacket',
-        price: 58
+        image: "/model_7.png",
+        title: "Yellow Jacket",
+        price: 58,
       },
       {
-        image: '/prod_1.png',
-        title: 'Leather Green Bag',
-        price: 28
+        image: "/prod_1.png",
+        title: "Leather Green Bag",
+        price: 28,
       },
       {
-        image: '/prod_2.png',
-        title: 'Gray Shoe',
-        price: 20
+        image: "/prod_2.png",
+        title: "Gray Shoe",
+        price: 20,
       },
       {
-        image: '/prod_3.png',
-        title: 'Blue Shoe High Heels',
-        price: 28
-      }
-    ]);
-  }, []);
+        image: "/prod_3.png",
+        title: "Blue Shoe High Heels",
+        price: 28,
+      },
+    ])
+  }, [])
 
   return (
     <Container fluid="sm">
@@ -49,18 +49,30 @@ const FavoriteProducts = () => {
           <h3>Wishlist</h3>
         </Col>
       </Row>
-      <div className="d-flex flex-wrap gap-5 justify-content-around">
+      <div className="d-flex flex-wrap gap-5 justify-content-around pb-5">
         {productList.map((product: any, index: any) => {
-          return <ProductCard key={index} index={index} product={product} screen="home" />;
+          return (
+            <ProductCard
+              key={index}
+              index={index}
+              product={product}
+              screen="home"
+            />
+          )
         })}
       </div>
       {productList.length === 0 && (
-        <div className={`d-flex flex-wrap gap-5 justify-content-around ${styles.favoriteSection}`}>
-          <p>You don&apos;t have any products in the wishlist. Go to shop to add favorite products.</p>
+        <div
+          className={`d-flex flex-wrap gap-5 justify-content-around ${styles.favoriteSection}`}
+        >
+          <p>
+            You don&apos;t have any products in the wishlist. Go to shop to add
+            favorite products.
+          </p>
         </div>
       )}
     </Container>
-  );
-};
+  )
+}
 
-export default FavoriteProducts;
+export default FavoriteProducts
--- src/app/components/PopularProducts.tsx
+++ src/app/components/PopularProducts.tsx
@@ -1,60 +1,69 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-'use client';
-import React, { useEffect, useState } from 'react';
-import { Col, Container, Row } from 'reactstrap';
-import ProductCard from './ProductCard';
+"use client"
+import React, { useEffect, useState } from "react"
+import { Col, Container, Row } from "reactstrap"
+import ProductCard from "./ProductCard"
 
 const PopularProducts = () => {
-  const [productList, setProductList] = useState<any>([]);
+  const [productList, setProductList] = useState<any>([])
   useEffect(() => {
     setProductList([
       {
-        image: '/model_1.png',
-        title: 'Smooth Cloth',
-        price: 28
+        image: "/model_1.png",
+        title: "Smooth Cloth",
+        price: 28,
       },
       {
-        image: '/model_5.png',
-        title: 'Denim Jacket',
-        price: 28
+        image: "/model_5.png",
+        title: "Denim Jacket",
+        price: 28,
       },
       {
-        image: '/model_7.png',
-        title: 'Yellow Jacket',
-        price: 58
+        image: "/model_7.png",
+        title: "Yellow Jacket",
+        price: 58,
       },
       {
-        image: '/prod_1.png',
-        title: 'Leather Green Bag',
-        price: 28
+        image: "/prod_1.png",
+        title: "Leather Green Bag",
+        price: 28,
       },
       {
-        image: '/prod_2.png',
-        title: 'Gray Shoe',
-        price: 20
+        image: "/prod_2.png",
+        title: "Gray Shoe",
+        price: 20,
       },
       {
-        image: '/prod_3.png',
-        title: 'Blue Shoe High Heels',
-        price: 28
-      }
-    ]);
-  }, []);
+        image: "/prod_3.png",
+        title: "Blue Shoe High Heels",
+        price: 28,
+      },
+    ])
+  }, [])
 
   return (
     <Container fluid="sm">
       <Row xs="1" className="py-5">
         <Col>
-          <h4 className="text-black text-left text-uppercase fw-medium border-start border-primary border-4 px-3">Popular Products</h4>
+          <h4 className="text-black text-left text-uppercase fw-medium border-start border-primary border-4 px-3">
+            Popular Products
+          </h4>
         </Col>
       </Row>
-      <div className="d-flex flex-wrap gap-5 justify-content-around">
+      <div className="d-flex flex-wrap gap-5 justify-content-around pb-5">
         {productList.map((product: any, index: any) => {
-          return <ProductCard key={index} index={index} product={product} screen="home" />;
+          return (
+            <ProductCard
+              key={index}
+              index={index}
+              product={product}
+              screen="home"
+            />
+          )
         })}
       </div>
     </Container>
-  );
-};
+  )
+}
 
-export default PopularProducts;
+export default PopularProducts
