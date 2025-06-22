783613fcee382843b47559b77a57e6cf19e5c285
shubhamvinnovate <shubham.sutar@vinnovatetechnologies.com>
2025-06-22 23:46:02 +0530

    server cors error fix

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ var express = require("express")
 var path = require("path")
 var cookieParser = require("cookie-parser")
 var logger = require("morgan")
+var cors = require("cors")
 
 var indexRouter = require("./routes/index")
 var usersRouter = require("./routes/users")
@@ -12,12 +13,13 @@ var app = express()
 app.set("view engine", "ejs")
 
 app.use(logger("dev"))
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, "public")))
 
 app.use("/", indexRouter)
-app.use("/users", usersRouter)
+app.use("/user", usersRouter)
 
 module.exports = app
